db5d194b968837a7fd40a014270330c30048c826
pawellakomiec <s14094@pjwstk.edu.pl>
2017-11-08 15:49:47 +0100

    simple template added

--- .idea/index.php
+++ .idea/index.php
@@ -7,6 +7,20 @@
  */
 ?>
 
+<!doctype html>
+
+<html>
+<head>
+    <title>Movies & Series</title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <link rel="stylesheet" type="text/css" href="index.css">
+</head>
 
 
-main
+<body>
+<header id="pageHeader">Header</header>
+<article id="mainArticle">Article</article>
+<nav id="mainNav">Nav</nav>
+<div id="siteAds">Ads</div>
+<footer id="pageFooter">Footer</footer>
+</body>
